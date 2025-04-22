b1e26fc75882495d32bde5fc26ef61eac562e522
Tarequr Rahman <tarequrrahmansakil@gmai>
2025-04-23 00:05:40 +0600

    letcode upload1

--- letcode.py
+++ letcode.py
@@ -1,4 +1,4 @@
-# Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+# 1. Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
 nums=[2,4,6,4];
 
 def product(nums):
@@ -11,3 +11,23 @@ def product(nums):
                 ans[i] *= nums[j]
     return ans;
 print(product(nums))
+
+
+
+
+
+# 2. Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+my_number = [1, 0, 9, 0, 5, 0, 7,0, 8, 4]
+my_number.sort()
+# print(my_number)
+def my_function(my_number):
+    non_zero = [ x for x in my_number
+                 if x != 0]
+    zero = [0] * (len(my_number) - len(non_zero))
+    return non_zero + zero
+print(my_function(my_number))
+
+
+
+
